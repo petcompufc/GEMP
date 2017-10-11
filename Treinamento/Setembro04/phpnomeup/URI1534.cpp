d0836beb61b925a88dce21cc0487f8a6f63cdc64
Débora Moura <deboramoura@alu.ufc.br>
2017-10-11 12:40:41 -0300

    *

--- Treinamento/Setembro04/phpnomeup/URI1534.cpp
+++ Treinamento/Setembro04/phpnomeup/URI1534.cpp
@@ -2,7 +2,7 @@
 	URI1534.cpp - (URI) 1534 - Array 123
 
 	Problema do Roteiro 0 - Primeiros Problemas
-	
+
 	Link Original do problema: https://www.urionlinejudge.com.br/judge/pt/problems/view/1534
 	Link para Dicas da Solução: http://wiki.maratona.dcc.ufmg.br/index.php/URI1534
 
@@ -15,6 +15,25 @@ using namespace std;
 
 int main()
 {
-	
+	int n;
+    while (cin>>n){
+        int m[n][n];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (i==(n-1-j))
+                    m[i][j]=2;
+                else if (i==j)
+                    m[i][j]=1;
+                else
+                    m[i][j]=3;
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-1;j++){
+                cout << m[i][j];
+            }
+            cout << m[i][n-1] << endl;
+        }
+    }
 	return 0;
 }
