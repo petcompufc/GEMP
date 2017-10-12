959841c90c9f486c06857fa6511960fb9e07c906
Leonardo Abreu <leonardomkflush@gmail.com>
2017-10-12 16:14:52 -0300

    URI 1534 resolução

--- Treinamento/Setembro04/CounaoC/URI1534.cpp
+++ Treinamento/Setembro04/CounaoC/URI1534.cpp
@@ -13,8 +13,27 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
+	int N, **M, i, j;
+	while( cin >> N ){
+		M = new int*[N];
+		for( i=0; i<N; i++ )
+			M[i] = new int[N];
+		for( i=0; i<N; i++ ){
+			for( j=0; j<N; j++ ){
+				if( j == (N-1)-i ) M[i][j] = 2;
+				else if( j == i ) M[i][j] = 1;
+				else M[i][j] = 3;
+				cout << M[i][j];
+			}
+			cout << endl;
+		}
+		
+		for( i=0; i<N; i++ )
+			delete M[i];
+		delete M;
+	}
 	
 	return 0;
 }
+
