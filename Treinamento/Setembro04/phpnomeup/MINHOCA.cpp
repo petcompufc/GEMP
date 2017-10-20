36b867372729a79129f1f754518192c5a5019cad
Jonas Albuquerque <jonas_albuquerque@outlook.com.br>
2017-10-20 10:54:04 -0300

    MINHOCA

--- Treinamento/Setembro04/phpnomeup/MINHOCA.cpp
+++ Treinamento/Setembro04/phpnomeup/MINHOCA.cpp
@@ -2,13 +2,15 @@
 	MINHOCA.cpp - (SPOJ-BR) MINHOCA
 
 	Problema do Roteiro 0 - Primeiros Problemas
-	
+
 	Link Original do problema: http://br.spoj.com/problems/MINHOCA/
 	Link para Dicas da Solução: http://wiki.maratona.dcc.ufmg.br/index.php/MINHOCA
 
 	ATENÇÃO! Pedimos que não veja as dicas da Solução até ter quebrado bastante a cabeça tentando resolver o problema :)
 
 */
+/*
+//Solução - ÍTALO
 #include <iostream>
 
 using namespace std;
@@ -42,6 +44,47 @@ int main(){
 	}
 
 	cout << saida;
-	
+
 	return 0;
 }
+*/
+//Solução - JONAS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    int campo[n][m];
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            cin >> campo[i][j];
+        }
+    }
+
+    int maiorL=0,maiorC=0,totalL=0,totalC=0;
+    for (int i=0; i<n; i++){
+        maiorL=0;
+        for (int j=0; j<m; j++){
+            maiorL+=campo[i][j];
+        }
+        if (maiorL>totalL)
+            totalL=maiorL;
+    }
+    for (int i=0; i<m; i++){
+        maiorC = 0;
+        for (int j=0; j<n; j++){
+            maiorC+=campo[j][i];
+        }
+        if (maiorC>totalC)
+            totalC=maiorC;
+    }
+    if (totalC>totalL)
+        cout << totalC << endl;
+    else
+        cout << totalL << endl;
+    return 0;
+}
+
