8d10e369f264e139a17f6f712067ec9819226206
Matheus <matheussr_iron@hotmail.com>
2017-10-19 22:40:29 -0200

    URI 1435

--- Treinamento/Outubro02/thefloorjava/URI1435.cpp
+++ Treinamento/Outubro02/thefloorjava/URI1435.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 	URI1435.cpp - (URI) 1435 - Matriz Quadrada I
 
 	Problema do Roteiro 1 - Problemas Ad-Hoc
@@ -10,10 +10,58 @@
 
 */
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-	
-	return 0;
+    int N,i,j,contador,aux;
+
+    while ((cin>>N) && N!=0) {
+        int M[N][N];
+        for(i=0;i<N;i++){
+            for(j=0;j<N;j++){
+                M[i][j]=0;
+            }
+        }
+
+        aux=N;
+        contador=0;
+
+        if(aux==1||aux==2){
+            if(aux==1){
+                cout<<"  1"<<endl;
+                cout<<endl;
+            }
+            else{
+                cout<<"  1   1"<<endl;
+                cout<<"  1   1"<<endl;
+                cout<<endl;
+            }
+        }
+        else{
+
+            while((aux-2)>=0){
+                for(i=contador;i<aux;i++){
+                    for(j=contador;j<aux;j++){
+                        M[i][j]+=1;
+                    }
+                }
+                aux--;
+                contador++;
+            }
+
+            for(i=0;i<N;++i){
+                for(j=0;j<N;++j){
+                    cout<<setw(3);
+                    if(j+1==N) cout<<M[i][j]<<endl;
+                    else cout<<M[i][j]<<" ";
+                }
+            }
+            cout<<"\n";
+        }
+    }
+
+  return 0;
+
 }
