555c60686b1c12679350b8e9770306f1b9c7c0ab
Minho Menezes <minhomenezes@gmail.com>
2017-10-27 12:19:46 -0300

    Starting INTERLMG.cpp

--- Treinamento/Outubro02/thefloorjava/INTERLMG.cpp
+++ Treinamento/Outubro02/thefloorjava/INTERLMG.cpp
@@ -10,10 +10,33 @@
 
 */
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
+float eucDist(pair<float,float> po1, pair<float,float> po2) {
+	return sqrt(pow(po1.first-po2.first, 2) + pow(po1.second-po2.second, 2) );
+}
+
 int main() {
+	int N;
+	float aux1, aux2;
+	vector< pair<float, float> > cidade;
+
+	while(cin >> N && N != 0) {
+		cidade.clear();
+
+		for (int i = 0; i < N; ++i) {
+			cin >> aux1 >> aux2;
+			cidade.push_back(make_pair(aux1, aux2));
+		}
+
+		for (int i = 1; i < N; ++i)	{
+			cout << eucDist(cidade[i], cidade[i-1]) << endl;
+		}
+	}
 	
 	return 0;
 }
