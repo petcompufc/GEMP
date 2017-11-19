f39b59f3a1f16f388da9f88d6c7a8d4d7bfab899
danieç <edaybrasil@gmail.com>
2017-11-19 12:47:09 -0200

    Uploading Novembro01 solutions

--- Treinamento/Novembro01/soCnadaC/MIDDLE.cpp
+++ Treinamento/Novembro01/soCnadaC/MIDDLE.cpp
@@ -1,19 +1,37 @@
-﻿/*
+/*
 	MIDDLE.cpp - (PKU) Who's In The Middle?
-
 	Problema do Roteiro 2 - Estruturas de Dados
 	
 	Link Original do problema: http://poj.org/problem?id=2388
 	Link para Dicas da Solução: http://wiki.maratona.dcc.ufmg.br/index.php/PKU_2388
-
 	ATENÇÃO! Pedimos que não veja as dicas da Solução até ter quebrado bastante a cabeça tentando resolver o problema :)
-
 */
-#include <iostream>
+
+#include <iostream>     // cout
+#include <algorithm>    // sort
+#include <vector>       // vector
 
 using namespace std;
 
-int main() {
-	
-	return 0;
+int main () 
+{
+  	int n;
+  	int i ,aux,j,middle;
+  	vector<int> lista;
+
+  	cin >> n;
+  	middle = n/2;
+  
+  	for(i=1;i<=n;i++)
+  	{
+    	cin>> aux;
+    	lista.push_back(aux);
+  	}
+
+  	sort (lista.begin(), lista.end());
+  
+  	cout << lista[middle];
+  	cout << '\n';
+
+  	return 0;
 }
