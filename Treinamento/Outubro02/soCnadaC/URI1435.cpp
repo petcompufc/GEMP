601084f94270fb697898991d11784f931dd97593
danieç <edaybrasil@gmail.com>
2017-12-21 09:55:57 -0300

    Add files via upload

--- Treinamento/Outubro02/soCnadaC/URI1435.cpp
+++ Treinamento/Outubro02/soCnadaC/URI1435.cpp
@@ -1,19 +1,51 @@
-/*
-	URI1435.cpp - (URI) 1435 - Matriz Quadrada I
+#include <stdio.h>
+#include <math.h>
 
-	Problema do Roteiro 1 - Problemas Ad-Hoc
-	
-	Link Original do problema: https://www.urionlinejudge.com.br/judge/pt/problems/view/1435
-	Link para Dicas da Solução: http://wiki.maratona.dcc.ufmg.br/index.php/URI1435
+int main(int argc, char const *argv[])
+{
+	int entrada;
+	scanf("%d",&entrada);
+	while(entrada!=0)
+	{
+		int v[entrada][entrada];
+		int i , j;
+		int h;
+		int teto;
 
-	ATENÇÃO! Pedimos que não veja as dicas da Solução até ter quebrado bastante a cabeça tentando resolver o problema :)
+		if (entrada%2==1)
+		{
+			teto=(entrada/2)+1;
+		}
+		else
+		{
+			teto=entrada/2;
+		}
 
-*/
-#include <iostream>
 
-using namespace std;
+		for (h = 1; h <= teto; h++)
+		{
+				for ( i = h-1; i <= entrada-h; i++)
+				{
+					for ( j = h-1; j <= entrada-h; j++)
+					{
+						v[i][j]=h;
+					}
+				}
+		}
 
-int main() {
-	
+int g , r;
+for(g=0; g<entrada; g++)
+            {
+                for(r=0; r<entrada; r++)
+                {
+                    if(r==0)
+                        printf("%3d",v[g][r]);
+                    else printf(" %3d",v[g][r]);
+                }
+                printf("\n");
+            }
+		printf("\n");
+		scanf("%d",&entrada);
+	}
 	return 0;
 }
